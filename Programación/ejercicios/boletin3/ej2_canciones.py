f302b663e8c9b64d4596d50e55a59cdf0dc0fc57
juanrm27 <juanreinam27@gmail.com>
2022-02-17 14:49:49 +0100

    jueves

--- Programación/ejercicios/boletin3/ej2_canciones.py
+++ Programación/ejercicios/boletin3/ej2_canciones.py
@@ -5,7 +5,7 @@ class Cancion():
 
 class CD():
     def __init__(self, lista = []) -> None:
-        self.lista_caciones = lista
+        self.__lista_caciones = lista
         self.contador = len(lista)
 
     def agrega_cancion(self, tema):
@@ -16,3 +16,14 @@ class CD():
        self.lista_caciones.pop(indice)
        self.contador = len(self.lista_caciones)
     
+    @property
+    def numero_canciones(self):
+        return self.contador
+    
+    @property
+    def canciones(self):
+        return self.__lista_caciones
+
+    @canciones.setter
+    def canciones(self,lista_canciones):
+        self.__lista_caciones = lista_canciones
